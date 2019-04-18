8d3d6fb7777238296ba9b8a0c405c3ef86c4ebde
Ian Fisher <iafisher@protonmail.com>
2019-04-18 11:44:55 -0400

    Implement basic, useless REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    loop {
+        print!("? ");
+        io::stdout().flush().expect("Failed to flush standard output");
+        io::stdin().read_line(&mut line).expect("Failed to read line");
+
+        let trimmed = line.as_str().trim();
+        if trimmed == ".exit" {
+            break;
+        } else {
+            println!("Error: unrecognized command `{}`", trimmed)
+        }
+
+        line.clear();
+    }
 }
