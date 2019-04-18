d7955815a97f46f50c1027889eb9b67062f27456
Ian Fisher <iafisher@protonmail.com>
2019-04-18 18:23:56 -0400

    Try and fail to implement table data structure

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::prelude::*;
 
 
 fn main() {
+    let mut table = Table { nrows: 0, pages: [None; TABLE_MAX_PAGES]};
+
     let mut line = String::new();
     loop {
         print!("? ");
@@ -20,7 +22,12 @@ fn main() {
             }
         } else {
             match prepare_statement(trimmed) {
-                Some(statement) => execute_statement(&statement),
+                Some(statement) => {
+                    match execute_statement(&statement, &mut table) {
+                        Ok(_) => (),
+                        Err(e) => println!("Error: {}", e),
+                    }
+                }
                 None => println!("Error: could not parse statement `{}`", trimmed),
             }
         }
@@ -51,7 +58,36 @@ struct Row<'a> {
 
 fn prepare_statement(command: &str) -> Option<Statement> {
     if command.starts_with("insert") {
-        Some(Statement { kind: StatementKind::Insert, row_to_insert: None })
+        let mut iter = command.split_ascii_whitespace();
+        iter.next();
+
+        let e1 = iter.next();
+        let e2 = iter.next();
+        let e3 = iter.next();
+        let e4 = iter.next();
+
+        match (e1, e2, e3, e4) {
+            (Some(word1), Some(word2), Some(word3), None) => {
+                match word1.parse::<u32>() {
+                    Ok(n) => {
+                        return Some(Statement {
+                            kind: StatementKind::Insert,
+                            row_to_insert: Some(Row {
+                                id: n,
+                                username: word2,
+                                email: word3,
+                            }),
+                        });
+                    },
+                    Err(_) => {
+                        return None;
+                    },
+                }
+            },
+            _ => {
+                return None;
+            },
+        }
     } else if command.starts_with("select") {
         Some(Statement { kind: StatementKind::Select, row_to_insert: None })
     } else {
@@ -60,7 +96,51 @@ fn prepare_statement(command: &str) -> Option<Statement> {
 }
 
 
-fn execute_statement(statement: &Statement) {
+const TABLE_MAX_PAGES: usize = 100;
+const PAGE_SIZE: usize = 4096;
+const ROW_SIZE: usize = 291;
+const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
+const TABLE_MAX_ROWS: usize = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+
+type Page = [u8; PAGE_SIZE];
+struct Table<'a> {
+    nrows: usize,
+    pages: [Option<&'a mut Page>; TABLE_MAX_PAGES],
+}
+
+
+fn execute_statement(statement: &Statement, table: &mut Table) -> Result<(), &'static str> {
+    if table.nrows >= TABLE_MAX_ROWS {
+        return Err("table is full");
+    }
+
+    let (page_num, offset) = row_slot(&mut table, table.nrows);
+    serialize_row(
+        &statement.row_to_insert.unwrap(),
+        &mut table.pages[page_num].unwrap(),
+        offset
+    );
+    Ok(())
+}
+
+
+fn serialize_row(row: &Row, destination: &mut Page, offset: usize) {
+
+}
+
+
+fn row_slot<'a>(table: &'a mut Table, row_num: usize) -> (usize, usize) {
+    let page_num = row_num / ROWS_PER_PAGE;
+
+    if table.pages[page_num].is_none() {
+        table.pages[page_num] = Some(&mut [0; PAGE_SIZE]);
+    }
+
+    let mut page = table.pages[page_num].unwrap();
+
+    let row_offset = row_num % ROWS_PER_PAGE;
+    return (page_num, row_offset * ROW_SIZE);
 }
 
 
