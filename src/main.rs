341909c2fe13a452a244577554f017f57b67991c
Ian Fisher <iafisher@protonmail.com>
2019-04-18 12:43:57 -0400

    Scaffold out logic for preparing and executing statements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::io::prelude::*;
 
+
 fn main() {
     let mut line = String::new();
     loop {
@@ -9,12 +10,71 @@ fn main() {
         io::stdin().read_line(&mut line).expect("Failed to read line");
 
         let trimmed = line.as_str().trim();
-        if trimmed == ".exit" {
-            break;
+        if trimmed.starts_with(".") {
+            match do_meta_command(trimmed) {
+                MetaCommandResult::Success => (),
+                MetaCommandResult::Exit => break,
+                MetaCommandResult::Unrecognized => {
+                    println!("Error: unrecognized meta-command `{}`", trimmed);
+                }
+            }
         } else {
-            println!("Error: unrecognized command `{}`", trimmed)
+            match prepare_statement(trimmed) {
+                Some(statement) => execute_statement(&statement),
+                None => println!("Error: could not parse statement `{}`", trimmed),
+            }
         }
 
         line.clear();
+    }
+}
+
+
+struct Statement<'a> {
+    kind: StatementKind,
+    row_to_insert: Option<Row<'a>>,
+}
+
+
+enum StatementKind {
+    Insert,
+    Select,
+}
+
+
+struct Row<'a> {
+    id: u32,
+    username: &'a str,
+    email: &'a str,
+}
+
+
+fn prepare_statement(command: &str) -> Option<Statement> {
+    if command.starts_with("insert") {
+        Some(Statement { kind: StatementKind::Insert, row_to_insert: None })
+    } else if command.starts_with("select") {
+        Some(Statement { kind: StatementKind::Select, row_to_insert: None })
+    } else {
+        None
+    }
+}
+
+
+fn execute_statement(statement: &Statement) {
+}
+
+
+enum MetaCommandResult {
+    Success,
+    Exit,
+    Unrecognized,
+}
+
+
+fn do_meta_command(command: &str) -> MetaCommandResult {
+    if command == ".exit" {
+        return MetaCommandResult::Exit;
+    } else {
+        return MetaCommandResult::Unrecognized;
     }
 }
