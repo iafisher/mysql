0de9f66945deda7ed78c4f3cb3663533ecbaf9e6
Ian Fisher <iafisher@protonmail.com>
2019-04-18 20:17:29 -0400

    Thank you to the good people of Stack Overflow

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use std::io;
 use std::io::prelude::*;
+use std::iter;
+use std::mem;
+use std::ptr;
+
+
+// Initialize an array with non-Copy types.
+// Courtesy of https://stackoverflow.com/questions/28656387/
+macro_rules! make_array {
+    ($n:expr, $constructor:expr) => {{
+        let mut items: [_; $n] = mem::uninitialized();
+        for (i, place) in items.iter_mut().enumerate() {
+            ptr::write(place, $constructor(i));
+        }
+        items
+    }}
+}
 
 
 fn main() {
-    let mut table = Table { nrows: 0, pages: [None; TABLE_MAX_PAGES]};
+    let mut table = Table {
+        nrows: 0, 
+        // pages: [None; TABLE_MAX_PAGES]
+        pages: unsafe { make_array!(TABLE_MAX_PAGES, |_| None) },
+    };
 
     let mut line = String::new();
     loop {
@@ -39,7 +59,7 @@ fn main() {
 
 struct Statement<'a> {
     kind: StatementKind,
-    row_to_insert: Option<Row<'a>>,
+    row_to_insert: Option<Box<Row<'a>>>,
 }
 
 
@@ -48,6 +68,9 @@ enum StatementKind {
     Select,
 }
 
+
+const ROW_USERNAME_SIZE: usize = 32;
+const ROW_EMAIL_SIZE: usize = 255;
 
 struct Row<'a> {
     id: u32,
@@ -70,13 +93,10 @@ fn prepare_statement(command: &str) -> Option<Statement> {
             (Some(word1), Some(word2), Some(word3), None) => {
                 match word1.parse::<u32>() {
                     Ok(n) => {
+                        let row = Row { id: n, username: word2, email: word3 };
                         return Some(Statement {
                             kind: StatementKind::Insert,
-                            row_to_insert: Some(Row {
-                                id: n,
-                                username: word2,
-                                email: word3,
-                            }),
+                            row_to_insert: Some(Box::new(row)),
                         });
                     },
                     Err(_) => {
@@ -103,10 +123,9 @@ const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
 const TABLE_MAX_ROWS: usize = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 
-type Page = [u8; PAGE_SIZE];
 struct Table<'a> {
     nrows: usize,
-    pages: [Option<&'a mut Page>; TABLE_MAX_PAGES],
+    pages: [Option<Box<&'a mut [u8]>>; TABLE_MAX_PAGES],
 }
 
 
@@ -115,7 +134,7 @@ fn execute_statement(statement: &Statement, table: &mut Table) -> Result<(), &'s
         return Err("table is full");
     }
 
-    let (page_num, offset) = row_slot(&mut table, table.nrows);
+    let (page_num, offset) = row_slot(table, table.nrows);
     serialize_row(
         &statement.row_to_insert.unwrap(),
         &mut table.pages[page_num].unwrap(),
@@ -125,8 +144,22 @@ fn execute_statement(statement: &Statement, table: &mut Table) -> Result<(), &'s
 }
 
 
-fn serialize_row(row: &Row, destination: &mut Page, offset: usize) {
+fn serialize_row(row: &Row, destination: &mut [u8], offset: usize) {
+    let id_bytes = row.id.to_be_bytes();
+    destination[offset] = id_bytes[0];
+    destination[offset+1] = id_bytes[1];
+    destination[offset+2] = id_bytes[2];
+    destination[offset+3] = id_bytes[3];
 
+    let padding = iter::repeat(0).take(ROW_USERNAME_SIZE - row.username.len());
+    for (i, c) in row.username.bytes().chain(padding).enumerate() {
+        destination[offset+3+i] = c;
+    }
+
+    let padding = iter::repeat(0).take(ROW_EMAIL_SIZE - row.email.len());
+    for (i, c) in row.email.bytes().chain(padding).enumerate() {
+        destination[offset+3+ROW_USERNAME_SIZE+i] = c;
+    }
 }
 
 
@@ -134,10 +167,9 @@ fn row_slot<'a>(table: &'a mut Table, row_num: usize) -> (usize, usize) {
     let page_num = row_num / ROWS_PER_PAGE;
 
     if table.pages[page_num].is_none() {
-        table.pages[page_num] = Some(&mut [0; PAGE_SIZE]);
+        let page = &mut [0; PAGE_SIZE];
+        table.pages[page_num] = Some(Box::new(page));
     }
-
-    let mut page = table.pages[page_num].unwrap();
 
     let row_offset = row_num % ROWS_PER_PAGE;
     return (page_num, row_offset * ROW_SIZE);
